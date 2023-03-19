b5cb3de6ea352030b5157992c65ae3c5d513cfbb
Alexey <71568795+tantum-verde-forte@users.noreply.github.com>
2023-03-19 12:31:41 +0300

    Update settings.py

--- crypto_view/settings.py
+++ crypto_view/settings.py
@@ -146,4 +146,4 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ['web-production-219d2.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-219d2.up.railway.app']
